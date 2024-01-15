546022599ee94d29caae28f4bc2e8e7f8c7433c3
Sf-Shohag71 <shakhfarid71@gmail.com>
2024-01-15 13:44:55 +0600

    complete form template design

---
+++ script.js
@@ -0,0 +1,8 @@
+let form1 = document.getElementById("form1");
+let form2 = document.getElementById("form2");
+let form3 = document.getElementById("form3");
+
+let next1 = document.getElementById("next1");
+let next2 = document.getElementById("next2");
+let back1 = document.getElementById("back1");
+let back2 = document.getElementById("back2");
